756dadd6a363537c616c4f153b72c22a872c3bc1
Oidaho <76991612+Oidaho@users.noreply.github.com>
2025-03-23 20:54:32 +1000

    Стартовый скрипт

---
+++ start.py
@@ -0,0 +1,13 @@
+import uvicorn
+
+from configs import configs
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "app:service",
+        host="0.0.0.0",
+        port=8062,
+        reload=configs.DEBUG_MODE,
+        date_header=True,
+        use_colors=True,
+    )
