b98841ab0b2a55809d35eee352e5995ecb3ae12c
Oidaho <76991612+Oidaho@users.noreply.github.com>
2025-03-26 23:02:49 +1000

    Добавлено поле Preview

--- database/models.py
+++ database/models.py
@@ -7,15 +7,18 @@ from .engine import BaseORM
 
 
 class LearningText(BaseORM):
+    """ORM таблицы, содержащей информацию по обучающим текстам."""
+
     __tablename__ = "learning_texts"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     title = Column(String(100), nullable=False, unique=True)
     difficulty = Column(Integer, nullable=True, unique=False)
+    preview = Column(String(500), nullable=True, unique=False)
     value = Column(Text, nullable=False, unique=False)
     transcription = Column(Text, nullable=False, unique=False)
 
     __table_args__ = (
         Index("learning_text_title_idx", title, postgresql_using="hash"),
-        CheckConstraint(difficulty >= 0, name="check_difficulty_is_positive"),
+        CheckConstraint(difficulty >= 0, name="check_text_difficulty_is_positive"),
     )
