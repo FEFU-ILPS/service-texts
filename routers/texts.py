4933954a06269144741ecc94f26d37744a33fd93
Oidaho <76991612+Oidaho@users.noreply.github.com>
2025-04-08 23:08:03 +1000

    feature: Роут создания/добавления текстов (#1)
    Этот коммит добавляет роут, который создает/добавляет
    в ILPS новый обучающий текст.

--- routers/texts.py
+++ routers/texts.py
@@ -1,16 +1,19 @@
 from typing import Annotated, List
 from uuid import UUID
 
-from fastapi import APIRouter, Depends, Path, HTTPException, status
+from fastapi import APIRouter, Body, Depends, HTTPException, Path, status
 from sqlalchemy import select
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from database import get_db
 from database.models import LearningText
 from schemas.texts import (
-    LearningTextResponse,
-    DetailLearningTextResponse,
+    CreateLearningTextRequest,
+    CreateLearningTextResponse,
     DeleteLearningTextResponse,
+    DetailLearningTextResponse,
+    LearningTextResponse,
 )
 
 router = APIRouter()
@@ -43,6 +46,42 @@ async def get_text(
         )
 
     return DetailLearningTextResponse.model_validate(text)
+
+
+@router.post("/", summary="Добавить текст в систему")
+async def create_text(
+    data: Annotated[CreateLearningTextRequest, Body(...)],
+    db: AsyncSession = Depends(get_db),
+) -> CreateLearningTextResponse:
+    """Добавляет новый текст в систему."""
+
+    try:
+        text = LearningText(
+            title=data.title,
+            difficulty=data.difficulty,
+            preview=data.preview,
+            value=data.value,
+            transcription=data.transcription,
+        )
+        db.add(text)
+        await db.commit()
+        await db.refresh(text)
+
+    except IntegrityError:
+        await db.rollback()
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"{LearningText.__name__} with this data already exists.",
+        )
+
+    except Exception:
+        await db.rollback()
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"An error ocured while creating {LearningText.__name__}.",
+        )
+
+    return CreateLearningTextResponse.model_validate(text)
 
 
 @router.delete("/{uuid}", summary="Удалить текст из системы")
