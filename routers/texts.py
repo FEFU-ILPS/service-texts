2e8ac4f6d8ec09d187e8d34b3471995894fbe757
Oidaho <76991612+Oidaho@users.noreply.github.com>
2025-04-10 21:40:06 +1000

    refactor: Реорганизация пакета schemas

--- routers/texts.py
+++ routers/texts.py
@@ -8,7 +8,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from database import get_db
 from database.models import LearningText
-from schemas.texts import (
+from schemas import (
     CreateLearningTextRequest,
     CreateLearningTextResponse,
     DeleteLearningTextResponse,
---
+++ schemas/__init__.py
@@ -0,0 +1,19 @@
+from .schemas import (
+    CreateLearningTextRequest,
+    CreateLearningTextResponse,
+    DeleteLearningTextResponse,
+    DetailLearningTextResponse,
+    LearningTextResponse,
+    UpdateLearningTextRequest,
+    UpdateLearningTextResponse,
+)
+
+__all__ = (
+    "CreateLearningTextRequest",
+    "CreateLearningTextResponse",
+    "DeleteLearningTextResponse",
+    "DetailLearningTextResponse",
+    "LearningTextResponse",
+    "UpdateLearningTextRequest",
+    "UpdateLearningTextResponse",
+)
