65f1f2183760d478eb6c3543a5c26a0ffef64040
Oidaho <76991612+Oidaho@users.noreply.github.com>
2025-05-20 22:18:08 +1000

    style, refactor: Рефактор примеров

--- schemas/examples.py
+++ schemas/examples.py
@@ -1,23 +1,21 @@
-from typing import Final
-
-ID_EXAMPLES: Final[list[str]] = [
+ID_EXAMPLES = [
     "d08191a1-3a4f-48e3-be3b-fc3bb31536af",
     "3d5dc460-5db7-4dd0-9d4e-3cd825ebaebd",
 ]
 
-TITLE_EXAMPLES: Final[list[str]] = [
+TITLE_EXAMPLES = [
     "Моя прекрасная жизнь",
     "Как я провел лето",
     "История великого русского государства",
 ]
 
-VALUE_EXAMPLES: Final[list[str]] = [
+VALUE_EXAMPLES = [
     "I assume moisture will damage this ship's hull.",
     "Roy ignored the spurious data points in drawing the graph.",
     "Are holiday aprons available to us?",
 ]
 
-TRANSCRIPTION_EXAMPLES: Final[list[str]] = [
+TRANSCRIPTION_EXAMPLES = [
     "Dɑːɹk hæd hæd jɚ jɚ ʃi suːt suːt ʃi hæd jɚ jɚ ʃi",
     "hæd suːt jɚ jɚ ʃi dɑːɹk ʃi hæd ʃi dɑːɹk ʃi hæd hæd hæd suːt jɚ suːt",
 ]
